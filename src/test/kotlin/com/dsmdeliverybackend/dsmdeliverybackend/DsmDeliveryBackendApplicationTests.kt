bf0a9f3848f7073b6128639aa02dbff9f29264ee
Woonseok105 <yws3786@gmail.com>
2023-01-27 15:15:37 +0900

    🌧 :: yml 파일 생성

---
+++ src/test/kotlin/com/dsmdeliverybackend/dsmdeliverybackend/DsmDeliveryBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dsmdeliverybackend.dsmdeliverybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsmDeliveryBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
