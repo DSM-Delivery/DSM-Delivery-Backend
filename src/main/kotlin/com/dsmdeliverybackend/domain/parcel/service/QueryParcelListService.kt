5ead9dbaa810d22608d481eb2a42581f6d12d211
Woonseok105 <yws3786@gmail.com>
2023-02-11 02:30:08 +0900

    🌧 :: 택배 리스트 서비스 생성

---
+++ src/main/kotlin/com/dsmdeliverybackend/domain/parcel/service/QueryParcelListService.kt
@@ -0,0 +1,28 @@
+package com.dsmdeliverybackend.domain.parcel.service
+
+import com.dsmdeliverybackend.domain.parcel.domain.repository.ParcelRepository
+import com.dsmdeliverybackend.domain.parcel.presentation.dto.response.ParcelListResponse
+import com.dsmdeliverybackend.domain.parcel.presentation.dto.response.ParcelResponse
+import org.springframework.stereotype.Service
+
+@Service
+class QueryParcelListService (
+    private val parcelRepository: ParcelRepository
+) {
+
+    fun execute(): ParcelListResponse {
+        val parcelList = parcelRepository.queryParcelList()//TODO 여기서 에러
+
+        return ParcelListResponse(
+            parcelList = parcelList
+                .map {
+                    ParcelResponse(
+                        percelId = it.parcelId,
+                        userName = it.userName,
+                        userPhoneNumber = it.userPhoneNumber
+                    )
+                }
+        )
+    }
+
+}
