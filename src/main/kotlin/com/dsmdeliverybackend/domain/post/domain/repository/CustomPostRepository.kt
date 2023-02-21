bb33035354a826bd089259ab8d1ed9379cb58162
Woonseok105 <yws3786@gmail.com>
2023-02-21 17:04:51 +0900

    🌧 :: 라이더 공지 분리

--- src/main/kotlin/com/dsmdeliverybackend/domain/post/domain/repository/CustomPostRepository.kt
+++ src/main/kotlin/com/dsmdeliverybackend/domain/post/domain/repository/CustomPostRepository.kt
@@ -1,7 +1,14 @@
 package com.dsmdeliverybackend.domain.post.domain.repository
 
-import com.dsmdeliverybackend.domain.post.domain.repository.vo.QueryPostVO
+import com.dsmdeliverybackend.domain.post.domain.repository.vo.QueryOrderPostVO
+import com.dsmdeliverybackend.domain.post.domain.repository.vo.QueryPostDetailVO
+import com.dsmdeliverybackend.domain.post.domain.repository.vo.QueryRiderPostVO
+import java.util.UUID
 
 interface CustomPostRepository {
-    fun queryPostList(): List<QueryPostVO>
+    fun queryOrderPostList(): List<QueryOrderPostVO>
+
+    fun queryRiderPostList(): List<QueryRiderPostVO>
+
+    fun queryPostDetail(postId: UUID): QueryPostDetailVO
 }
