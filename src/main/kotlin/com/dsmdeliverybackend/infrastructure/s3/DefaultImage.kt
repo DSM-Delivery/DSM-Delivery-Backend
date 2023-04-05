b96589c0262891b8b21bec0d3ff45c1719cfd039
Woonseok105 <yws3786@gmail.com>
2023-04-05 23:33:43 +0900

    🌧 :: DeafultImage Url

--- src/main/kotlin/com/dsmdeliverybackend/infrastructure/s3/DefaultImage.kt
+++ src/main/kotlin/com/dsmdeliverybackend/infrastructure/s3/DefaultImage.kt
@@ -1,5 +1,7 @@
 package com.dsmdeliverybackend.infrastructure.s3
 
-class DefaultImage (
-    val DEFAULT_PROFILE_IMAGE: String
-)
+class DefaultImage {
+    companion object {
+        const val DEFAULT_PROFILE_IMAGE = "https://dsm-delivery-bucket.s3.ap-northeast-2.amazonaws.com/29d34872-ca35-43d9-a970-1f663ad38245"
+    }
+}
